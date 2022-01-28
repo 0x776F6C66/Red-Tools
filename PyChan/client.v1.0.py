dea12336ab3b9a40a45c618e23bf3396261dfbea
Parsz~ <96860148+P4rsz@users.noreply.github.com>
2022-01-28 18:00:08 +0300

    Update client.v1.0.py

--- PyChan/client.v1.0.py
+++ PyChan/client.v1.0.py
@@ -5,22 +5,23 @@ import sys
 if len(sys.argv) <2:
     print("Usage: client.py ip port ")
     exit()
-#banner.
-print("\n\t########  ###  ###  ########  ###  ###  ########  ###    ###")
-print("\t########  ###  ###  ########  ###  ###  ###  ###  ## #   ###")
-print("\t########  ###  ###  ##        ########  ###  ###  ### #  ###    client v1.0")
-print("\t########  ########  ##        ########  ########  ###  # ###    by P4rsz and W1nterFr3ak")
-print("\t####           ###  ########  ###  ###  ###  ###  ###   # ##    email: parsz@protonmail.com WinterFreak@protonmail.com")
-print("\t####      ########  ########  ###  ###  ###  ###  ###    ###\n")
+
+if __name__ == "__main__":
+    #banner.
+    print("\n\t===========================================================")
+    print("\t                 PYCHAN client v1.0")
+    print("\t              by P4rsz and W1nterFr3ak")
+    print("\t   email:parsz@protonmail.com,WinterFreak@protonmail.com")
+    print("\t============================================================\n")
 
 #host and port.
 host = sys.argv[1]
 port = int(sys.argv[2])
 
 #socket object and connect to host.
-server = socket.socket(socket.AF_INET)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client = server.connect((host, port))
 data = server.recv(1024)
 print(f"\t{data.decode()}")
 print("\n\tExiting.\n")
-exit()
+server.close()
