682a65396a8db4da53ea493aa6c74c3693193374
Parsz~ <96860148+P4rsz@users.noreply.github.com>
2022-04-24 17:36:21 +0300

    Add files via upload
    this is a base64 shift decoder and encoder

---
+++ b64.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+import base64
+import sys
+
+if len(sys.argv) != 4:
+    print("USAGE: <base64.py> <[-d]ecode/[-e]ncode> <shift> <string>")
+    print("Example: base64.py -e 50 abcdefgh")
+    exit()
+
+option = sys.argv[1]
+shift = int(sys.argv[2])
+string = sys.argv[3]
+
+def encode(string_):
+    for x in range(1, shift+1):
+        try:
+            encoded = base64.b64encode(bytes(string_.encode()))
+            string_ = encoded.decode()
+            print(f"Shift {x}: {string_}")
+        except:
+            exit()
+
+def decode(string_):
+    for x in range(1, shift+1):
+        try:
+            decoded = base64.b64decode(string_)
+            string_ = decoded.decode()
+            print(f"\nShift {x}: {string_}")
+        except:
+            exit()
+
+
+def main():
+    print("\n\t-- Base64 shift Decoder and Encoder -- \n")
+    if option == "-e":
+        encode(string)
+    elif option == "-d":
+        decode(string)
+    else:
+        print("Check your arguments.")
+        exit()
+
+if __name__ == "__main__":
+    main()
