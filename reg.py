231cba30f0fa0771b016e621cc708f86d401408e
wolf <96860148+wo1fbit@users.noreply.github.com>
2022-05-28 21:10:46 +0300

    Add files via upload

---
+++ reg.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+#@author:wolfbit
+
+import os
+import sys
+import getopt
+
+ip = False
+
+def usage():
+    print("usage: reg.py [options] file.")
+    print("")
+    print("Options")
+    print("-------")
+    print("-i              - search for Ip addresses.")
+    print("-f --file=file  - file to check.")
+    exit()
+
+def main():
+    global ip
+    global files
+
+    if not len(sys.argv[1:]):
+        usage()
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "f:hi", ["help", "ip", "file="])
+    except getopt.GetoptError as err:
+        print(str(err))
+        exit()
+
+    for o, a in opts:
+        if o in ['-h', '--help']:
+            usage()
+        elif o in ['-i', '--ip']:
+            ip = True
+        elif o in ["-f", "--file"]:
+            files = a
+            if not os.path.exists(files):
+                print("File doesn't exist. Exiting !!\n")
+                exit()
+
+    if ip:
+        print("\nIP addresses")
+        print("------------")
+        command = "grep -a -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' -o "+ files
+        os.system(command)
+
+if __name__ == "__main__":
+    main()
+    print("")
